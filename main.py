a12cf9ee8e27c75f49232f8ea20d2147b5025071
=Anurag <=anuragsnd@gmail.com>
2025-03-25 16:31:22 +0530

    odd even

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to even odd checker")
+number = int(input("Please enter a number\n"))
+if number % 2 ==0:
+    print(f"Number {number} is an  even number ")
+else:
+    print(f"Number {number} is an odd number")
