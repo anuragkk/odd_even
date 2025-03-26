1227183630c170edafb1e0485d74588f86ca9783
=Anurag <=anuragsnd@gmail.com>
2025-03-26 22:45:12 +0530

    modified

--- N_4_2.py
+++ N_4_2.py
@@ -2,7 +2,8 @@
 import random
 print("Welcome to who will pay the bill\n")
 names = ["Anurag", "Golu", "Mummy", "X", "Y", "Z"]
-# print(len(names))
-random_number = random.randint(0,5)
+random_number = random.randint(0,len(names)-1)
 who_is_paying= names[random_number]
 print(f"The person who will pay the bill today will be {who_is_paying}")
+
+
